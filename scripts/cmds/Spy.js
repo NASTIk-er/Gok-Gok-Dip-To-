3aed91477f9432c9fe620b86d4b92cf414149205
NASTIk-er <balermeta@gmail.com>
2025-07-06 11:48:24 +0600

    Update Spy.js

--- scripts/cmds/Spy.js
+++ scripts/cmds/Spy.js
@@ -9,18 +9,19 @@ const baseApiUrl = async () => {
 module.exports = {
   config: {
     name: "spy",
+    aliases: ["whoishe", "whoisshe", "whoami", "atake"],
     version: "1.0",
-    hasPermission: 0,
-    usePrefix: true,
-    credits: "Dipto",
-    description: "Get user information and profile photo",
+    role: 0,
+    author: "Dipto",
+    Description: "Get user information and profile photo",
     category: "information",
-    cooldowns: 10,
+    countDown: 10,
   },
 
-  run: async function ({
+  onStart: async function ({
     event,
-    Users,
+    message,
+    usersData,
     api,
     args,
   }) {
@@ -46,7 +47,7 @@ module.exports = {
           ? event.messageReply.senderID
           : uid2 || uid1;
     }
-    const response = await axios.get(
+    const response = await require("axios").get(
       `${await baseApiUrl()}/baby?list=all`
     );
     const dataa = response.data || { teacher: { teacherList: [] } };
@@ -57,7 +58,7 @@ module.exports = {
     }
 
     const userInfo = await api.getUserInfo(uid);
-    const avatarUrl = `https://graph.facebook.com/${uid}/picture?height=1500&width=1500&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`;
+    const avatarUrl = await usersData.getAvatarUrl(uid);
 
     let genderText;
     switch (userInfo[uid].gender) {
@@ -71,33 +72,33 @@ module.exports = {
         genderText = "𝙶𝚊𝚢🤷🏻‍♂️";
     }
 
-    const money = (await Users.get(uid)).money;
-const allUser = await Users.getAll(), rank = allUser.slice().sort((a, b) => b.exp - a.exp).findIndex(user => user.userID === uid) + 1, moneyRank = allUser.slice().sort((a, b) => b.money - a.money).findIndex(user => user.userID === uid) + 1;
+    const money = (await usersData.get(uid)).money;
+    const allUser = await usersData.getAll(), rank = allUser.slice().sort((a, b) => b.exp - a.exp).findIndex(user => user.userID === uid) + 1, moneyRank = allUser.slice().sort((a, b) => b.money - a.money).findIndex(user => user.userID === uid) + 1;
 
     const position = userInfo[uid].type;
 
     const userInformation = `
-╭─[ 𝐔𝐒𝐄𝐑 𝐈𝐍𝐅𝐎 ]
+╭────[ 𝐔𝐒𝐄𝐑 𝐈𝐍𝐅𝐎 ]
 ├‣ 𝙽𝚊𝚖𝚎: ${userInfo[uid].name}
-├‣ 𝙽𝚒𝚌𝚔𝙽𝚊𝚖𝚎: ${userInfo[uid].alternateName || "𝙽𝚘𝚗𝚎"}
+├‣ 𝙶𝚎𝚗𝚍𝚎𝚛: ${genderText}
 ├‣ 𝚄𝙸𝙳: ${uid}
 ├‣ 𝙲𝚕𝚊𝚜𝚜: ${position ? position?.toUpperCase() : "𝙽𝚘𝚛𝚖𝚊𝚕 𝚄𝚜𝚎𝚛🥺"}
 ├‣ 𝚄𝚜𝚎𝚛𝚗𝚊𝚖𝚎: ${userInfo[uid].vanity ? userInfo[uid].vanity : "𝙽𝚘𝚗𝚎"}
-├‣ 𝙶𝚎𝚗𝚍𝚎𝚛: ${genderText}
+├‣ 𝙿𝚛𝚘𝚏𝚒𝚕𝚎 𝚄𝚁𝙻: ${userInfo[uid].profileUrl}
 ├‣ 𝙱𝚒𝚛𝚝𝚑𝚍𝚊𝚢: ${userInfo[uid].isBirthday !== false ? userInfo[uid].isBirthday : "𝙿𝚛𝚒𝚟𝚊𝚝𝚎"}
-├‣ 𝙵𝚛𝚒𝚎𝚗𝚍 𝚠𝚒𝚝𝚑 𝚋𝚘𝚝: ${userInfo[uid].isFriend ? "𝚈𝚎𝚜✅" : "𝙽𝚘❎"}
-╰‣ 𝙿𝚛𝚘𝚏𝚒𝚕𝚎 𝚄𝚁𝙻: ${userInfo[uid].profileUrl}
+├‣ 𝙽𝚒𝚌𝚔𝙽𝚊𝚖𝚎: ${userInfo[uid].alternateName || "𝙽𝚘𝚗𝚎"}
+╰‣ 𝙵𝚛𝚒𝚎𝚗𝚍 𝚠𝚒𝚝𝚑 𝚋𝚘𝚝: ${userInfo[uid].isFriend ? "𝚈𝚎𝚜✅" : "𝙽𝚘❎"}
 
-╭─[ 𝐔𝐒𝐄𝐑 𝐒𝐓𝐀𝐓𝐒 ]
+╭─────[ 𝐔𝐒𝐄𝐑 𝐒𝐓𝐀𝐓𝐒 ]
 ├‣ 𝙼𝚘𝚗𝚎𝚢: $${formatMoney(money)}
 ├‣ 𝚁𝚊𝚗𝚔: #${rank}/${allUser.length}
 ├‣ 𝙼𝚘𝚗𝚎𝚢 𝚁𝚊𝚗𝚔: #${moneyRank}/${allUser.length}
 ╰‣ 𝙱𝚊𝚋𝚢 𝚝𝚎𝚊𝚌𝚑: ${babyTeach || 0}`;
-const avatarStream = (await require("axios").get(avatarUrl, { responseType: "stream" })).data;
-   api.sendMessage({
+
+    message.reply({
       body: userInformation,
-      attachment: avatarStream,
-    }, event.threadID, event.messageID);
+      attachment: await global.utils.getStreamFromURL(avatarUrl),
+    });
   },
 };
 
